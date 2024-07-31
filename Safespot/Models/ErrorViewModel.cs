36d6bf8cc4f94b1b4eed2e96425a75c118661231
Aziz Rakhmanov <zzrxmnv1002@gmail.com>
2024-08-01 00:08:46 +0500

    Add project files.

---
+++ Safespot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Safespot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
